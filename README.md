2243ba3c7420a72ea166abd100015bfca1314487
DOMI623 <ecalmax@gmail.com>
2025-10-06 13:13:38 -0400

    lista de productos

--- README.md
+++ README.md
@@ -9,3 +9,8 @@
  2. crear una cuenta de github
  3. conectar vs code con github
  4. publicar al repositorio
+5. Lista de productos
+ 1. Crear tipo de producto
+ 2. Crear arreglo de productos
+ 3. Añadir imágenes de productos
+ 4. Renderizar productos
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { sampleProducts } from "./data";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <header>
-        RD Amazon
-      </header>
+      <header>RD Amazon</header>
       <main>
-
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img src={product.image} alt={product.name} className="product-image" />
+              <h2>{product.name}</h2>
+              <p>RD${product.price}</p>
+            </li>
+          ))}
+        </ul>
       </main>
-      <footer>
-        Todos los derechos reservados
-      </footer>
+      <footer>Todos los derechos reservados</footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
