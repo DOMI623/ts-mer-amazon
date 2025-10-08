80327a2465af8b8a9db1f1fa7494e039823d4eea
DOMI623 <ecalmax@gmail.com>
2025-10-08 14:12:39 -0400

    utilizamos reract router dom para poder navegar a la oagina de productos desde el homepage, creando las rutas desde el main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App'
+import './index.css'
+import HomePage from './HomePage'
+import ProductPage from './pages/ProductPage'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} >
+    <Route index={true} element={<HomePage />} />
+    <Route path='product/:slug' element={<ProductPage />} /> 
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
